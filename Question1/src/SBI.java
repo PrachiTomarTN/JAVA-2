e58b973d296f0b34a21afe404d2cacddac92ee55
prachi <prachi.tomar@tothenew>
2022-09-29 09:43:41 +0530

    Java 2

---
+++ Question1/src/SBI.java
@@ -0,0 +1,26 @@
+public class SBI extends Bank {
+
+    private final double rateOfInterest = 7.3;
+
+    public SBI() {
+    }
+
+    public SBI(String accountNumber, String name, AccountType accountType) {
+        super(accountNumber, name, "SBI", accountType);
+    }
+
+    public String getDetails() {
+        return "Bank{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", name='" + name + '\'' +
+                ", bankName= 'SBI'" +
+                ", accountType=" + accountType +
+                ", rateOfInterest=" + rateOfInterest + "%" +
+                ", accountOpeningDate=" + accountOpeningDate +
+                ", balance=" + balance +
+                '}';
+    }
+
+}
+
+
